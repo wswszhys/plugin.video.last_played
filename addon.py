80e9a39e85866e24e4b52d0ba4c7383833735ae5
狮子吼 <wswszhys@163.com>
2025-08-27 12:37:07 +0800

    Update addon.py

--- addon.py
+++ addon.py
@@ -165,7 +165,9 @@ elif menu[0] == 'showlist':
             #if len(filename) > 3:
             #    filename = '/'.join(filename[3:])
             # filename = 'library://video'+ line['file']
-            filename = 'sources://video/' + line['file'].split('/')[2:-1]
+            filename = line['file'].split('/')[2:-1]
+            filename = '/'.join(filename)
+            filename = 'sources://video/' + filename 
             #xbmc.executebuiltin("ActivateWindow(Videos,"+filename+")")
             li = xbmcgui.ListItem(xbmcvfs.translatePath(filename))
             command = []
